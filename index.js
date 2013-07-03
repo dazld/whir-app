c4486fac0ed1c87b2c75926c16e938c5b1917450
Dan Peddle <dan.peddle@gmail.com>
2013-07-03 17:52:04 +0200

    update reps

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var WhirApp = require('whir');
-var bb = require('backbone');
 
 
 var app = new WhirApp({
@@ -10,6 +9,10 @@ var app = new WhirApp({
 app.factory.addTemplates({
 	base: function(){ return 'base'; }
 });
+
+/*app.addControllers({
+    //
+});*/
 
 app.factory.addRoutes({
 	'/':'home'
