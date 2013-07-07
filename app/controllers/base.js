48bb4744f3d3c53d2745fc494698811989e19bad
Dan Peddle <dan.peddle@gmail.com>
2013-07-08 00:23:01 +0200

    added name param

--- app/controllers/base.js
+++ app/controllers/base.js
@@ -2,9 +2,9 @@ var WhirController = require('whir/core/controllers/WhirController'),
 	_ = require('whir/node_modules/underscore');
 
 var MyC = WhirController.extend({
+	name: 'base',
 	routes: {},
 	initialize: function(a, b, c) {
-
 		
 	},
 	doStuff: function() {},
